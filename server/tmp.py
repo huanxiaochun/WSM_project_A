cc8f0b77b46d55a3ff9221678463e4f18f0e9131
qaj <1347530792@qq.com>
2020-06-09 21:59:02 +0800

    Delete tmp.py

--- server/tmp.py
+++
@@ -1,6 +0,0 @@
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
-
-print(fuzz.ratio("上海徐汇人民法院", "上海市徐汇区人民法院"))
-print(fuzz.ratio("上海市徐汇区人民法院", "上海市徐汇区人民法院"))
-print(fuzz.ratio("(2017)沪 0112 执 5984 号", "(2017)沪 0112 执 5983 号"))
